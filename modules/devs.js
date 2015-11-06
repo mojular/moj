f198074c66070f468ce3afce59847a6e94dfe37b
Tyom Semonov <inspiro@gmail.com>
2015-11-06 15:13:42 +0000

    Refactor modules

--- modules/devs.js
+++ modules/devs.js
@@ -1,12 +1,9 @@
 'use strict';
 
 // Invite interested developers to join us
-
-var log = require('mojular').heisenberg.log;
-
 exports.Devs = {
   init: function () {
-    log([
+    this.base.log([
       '      ___          ___       ___\n     /__/\\        /  /\\     /  /\\\n    |  |::\\      /  /::\\   /  /:/\n    |  |:|:\\    /  /:/\\:\\ /__/::\\\n  __|__|:|\\:\\  /  /:/  \\:\\\\__\\/\\:\\\n /__/::::| \\:\\/__/:/ \\__\\:\\  \\  \\:\\\n \\  \\:\\~~\\__\\/\\  \\:\\ /  /:/   \\__\\:\\\n  \\  \\:\\       \\  \\:\\  /:/    /  /:/\n   \\  \\:\\       \\  \\:\\/:/    /__/:/\n    \\  \\:\\       \\  \\::/     \\__\\/\n     \\__\\/        \\__\\/',
       '\n\nLike what you see? Want to make a difference?',
       '\n\nFind out how we\'re making the Ministry Of Justice Digital by Default.',
